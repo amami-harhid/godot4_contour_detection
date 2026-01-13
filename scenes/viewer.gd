20cf15d278683ad72a2f39ad5702cbdfbfab2b88
amami-harhid <amami-thida100@outlook.jp>
2026-01-13 20:05:36 +0900

    update

--- scenes/viewer.gd
+++ scenes/viewer.gd
@@ -9,8 +9,6 @@ func _test01()->void:
 	
 	var _texture:ImageTexture = ImageTexture.new()
 	var _image:Image = Image.create(
-#		org_image.get_size().x+10,
-#		org_image.get_size().y+10, 
 		org_image.get_size().x,
 		org_image.get_size().y, 
 		false, Image.FORMAT_RGBA8)
@@ -30,7 +28,7 @@ func _test01()->void:
 			_image.set_pixel(_pos.x, _pos.y, Color(0,0,0,1))
 			_texture.set_image(_image)
 			self.texture = _texture	
-			await get_tree().create_timer(0.001).timeout
+			#await get_tree().create_timer(0.001).timeout
 	print("count=",count)
 func _custom_sort(a:Cell, b:Cell) -> bool:
 	if a.parent < b.parent:
